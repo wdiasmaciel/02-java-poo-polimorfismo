6d612c128e493fd197673955cc561ba6e49f709c
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-04-02 21:48:35 +0000

    Exemplo

---
+++ principal/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Principal {
+    static int num = 1;
+    
+	public static void main(String[] args) {
+		System.out.println("Número:" + num);
+		num = num + 1;
+		System.out.println("Número:" + num);
+	}
+}
